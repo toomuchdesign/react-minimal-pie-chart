8e4143f82d1fe52cb8f04bae8ec36262e91a79b8
Andrea Carraro <andrea.carraro@mosaicoon.com>
2017-03-11 20:01:56 +0100

    Introduce actual component

---
+++ index.js
@@ -0,0 +1,115 @@
+import React, { PropTypes } from 'react';
+import cornerArc from 'svg-arc-corners';
+
+const VIEWBOX_SIZE = 100;
+const VIEWBOX_HALF_SIZE = VIEWBOX_SIZE / 2;
+
+const sumValues = data => data.reduce((acc, dataEntry) => acc + dataEntry.value, 0);
+
+const evaluateDegreesFromValues = (data, totalAngle) => {
+  const total = sumValues(data);
+
+  // Append "degrees" property into each data entry
+  return data.map(dataEntry => Object.assign(
+    { degrees: (dataEntry.value / total) * totalAngle },
+    dataEntry,
+  ));
+};
+
+const makeSingleSegment = (startAngle = 0, endAngle = 0, key, color, lineWidth, cornerRadius) => {
+  // Let svg-arc-corners evaluate "d" value
+  // https://github.com/w8r/svg-arc-corners/tree/v1.0.4
+  const pathD = cornerArc(
+    [VIEWBOX_HALF_SIZE, VIEWBOX_HALF_SIZE],   // Center chart to the middle of viewBox
+    VIEWBOX_HALF_SIZE,                        // Radius
+    startAngle,
+    endAngle,
+    lineWidth,
+    cornerRadius,
+  );
+
+  return (
+    <path
+      d={pathD}
+      key={key}
+      fill={color}
+    />
+  );
+};
+
+const makeSegments = (data, props) => {
+  let degreesAccumulator = 0;
+
+  return data.map((dataEntry, index) => {
+    const segment = makeSingleSegment(
+      degreesAccumulator + props.startAngle,
+      degreesAccumulator + props.startAngle + dataEntry.degrees,
+      dataEntry.key || index,
+      dataEntry.color,
+      props.lineWidth,
+      props.cornerRadius,
+    );
+
+    // Keep track of how many degrees have already been taken
+    degreesAccumulator += dataEntry.degrees;
+    return segment;
+  });
+};
+
+export default function CheapGoalPie({ style, ...props }) {
+  if (props.data === undefined) {
+    return null;
+  }
+
+  const normalizedData = evaluateDegreesFromValues(props.data, props.endAngle - props.startAngle);
+
+  return (
+    <div
+      style={Object.assign({ height: `${props.size}px` }, style)}
+      {...props}
+    >
+      <svg
+        viewBox={`0 0 ${VIEWBOX_SIZE} ${VIEWBOX_SIZE}`}
+        width="100%"
+        height="100%"
+      >
+        {makeSegments(normalizedData, props)}
+      </svg>
+    </div>
+  );
+}
+
+CheapGoalPie.displayName = 'CheapGoalPie';
+
+CheapGoalPie.propTypes = {
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      value: PropTypes.number,
+      key: PropTypes.oneOfType([
+        PropTypes.number,
+        PropTypes.string,
+      ]),
+      color: PropTypes.string,
+      degrees: PropTypes.number,
+    }),
+  ),
+  style: PropTypes.arrayOf(
+    PropTypes.oneOfType([
+      PropTypes.number,
+      PropTypes.string,
+    ]),
+  ),
+  size: PropTypes.number,
+  lineWidth: PropTypes.number,
+  cornerRadius: PropTypes.number,
+  startAngle: PropTypes.number,
+  endAngle: PropTypes.number,
+};
+
+CheapGoalPie.defaultProps = {
+  size: 100,
+  startAngle: 0,
+  endAngle: 360,
+  lineWidth: 10,
+  cornerRadius: 0,
+};
