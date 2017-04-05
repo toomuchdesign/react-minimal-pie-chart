7fb1b59d5f82fa6b8b596965d5b815dfd4c124c9
Andrea Carraro <me@andreacarraro.it>
2017-04-05 16:50:09 +0200

    Improve partialCircle readibility

--- src/partialCircle.js
+++ src/partialCircle.js
@@ -1,13 +1,13 @@
 // Taken from: https://github.com/derhuerst/svg-partial-circle
-// TODO: fix/extend svg-partial-circle lib
+// @TODO: fix/extend svg-partial-circle lib
 export default function partialCircle(cx, cy, r, start, end) {
   const length = end - start;
   if (length === 0) return [];
 
-  const fromX = cx + r * Math.cos(start);
-  const fromY = cy + r * Math.sin(start);
-  const toX = cx + r * Math.cos(end);
-  const toY = cy + r * Math.sin(end);
+  const fromX = r * Math.cos(start) + cx;
+  const fromY = r * Math.sin(start) + cy;
+  const toX = r * Math.cos(end) + cx;
+  const toY = r * Math.sin(end) + cy;
   const large = Math.abs(length) <= Math.PI ? '0' : '1';
   const sweep = length < 0 ? '0' : '1';
 
