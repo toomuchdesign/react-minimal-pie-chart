827eb489904b697eaa1d8edb284005af0012a48a
Andrea Carraro <me@andreacarraro.it>
2017-06-30 17:40:06 +0200

    Update storybook to v.3

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,4 @@
-import { configure } from '@kadira/storybook';
+import { configure } from '@storybook/react';
 function loadStories() {
   require('../stories/index.js');
   // You can require as many stories as you need.
