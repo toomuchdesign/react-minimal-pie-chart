0eaa8164c0994b8dcf3ea2e546bb8e026bce1afa
Andrea Carraro <me@andreacarraro.it>
2019-02-24 13:11:13 +0100

    Reorganize storybook demo

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,5 @@
 import { configure } from '@storybook/react';
+
 function loadStories() {
   require('../stories/index.js');
   // You can require as many stories as you need.
---
+++ stories/LoadingIndicatorStory.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import PieChart from '../src/index.js';
+
+class LoadingIndicatorStory extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      percentage: 20,
+    };
+
+    this.handleRangeChange = this.handleRangeChange.bind(this);
+  }
+
+  handleRangeChange(e) {
+    const newValue = e.target.value;
+    this.setState(() => ({ percentage: Number(newValue) }));
+  }
+
+  render() {
+    return (
+      <div>
+        <PieChart
+          data={[{ value: 1, key: 1, color: '#E38627' }]}
+          reveal={this.state.percentage}
+          lineWidth={20}
+          animate
+        />
+        Reveal: {this.state.percentage}%
+        <input
+          type="range"
+          min="0"
+          max="100"
+          step="1"
+          value={this.state.percentage}
+          style={{ width: '100%' }}
+          onChange={this.handleRangeChange}
+        />
+      </div>
+    );
+  }
+}
+
+export default LoadingIndicatorStory;
