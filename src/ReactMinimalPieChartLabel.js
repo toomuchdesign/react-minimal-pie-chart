aca7ea8f5fb19fec6907c989205e44bd968b97a1
Andrea Carraro <me@andreacarraro.it>
2019-02-25 18:00:00 +0100

    Do not expose <ReactMinimalPieChartLabel>

--- src/ReactMinimalPieChartLabel.js
+++ src/ReactMinimalPieChartLabel.js
@@ -3,22 +3,18 @@ import PropTypes from 'prop-types';
 import { dataPropType } from './propTypes';
 
 export default function ReactMinimalPieChartLabel({
-  data,
-  dataIndex,
+  data, // eslint-disable-line no-unused-vars
+  dataIndex, // eslint-disable-line no-unused-vars
   color,
-  children,
   ...props
 }) {
   return (
     <text
       textAnchor="middle"
       alignmentBaseline="middle"
-      className="react-minimal-pie-chart-label"
       fill={color}
       {...props}
-    >
-      {children(data, dataIndex, props)}
-    </text>
+    />
   );
 }
 
@@ -27,6 +23,5 @@ ReactMinimalPieChartLabel.displayName = 'ReactMinimalPieChartLabel';
 ReactMinimalPieChartLabel.propTypes = {
   data: dataPropType,
   dataIndex: PropTypes.number,
-  children: PropTypes.func.isRequired,
   color: PropTypes.string,
 };
--- src/index.js
+++ src/index.js
@@ -1,2 +1,1 @@
 export { default } from './ReactMinimalPieChart';
-export { default as PieChartLabel } from './ReactMinimalPieChartLabel';
