3dc63ffef361f39834226d8b18a06a538ced08ff
Andrea Carraro <me@andreacarraro.it>
2019-03-10 01:39:30 +0100

    Update to Babel 7

---
+++ .babelrc.js
@@ -0,0 +1,16 @@
+const test = process.env.NODE_ENV === 'test';
+
+module.exports = {
+  presets: [
+    [
+      '@babel/env',
+      {
+        modules: test ? 'cjs' : false,
+        loose: true,
+      },
+    ],
+  ],
+  plugins: [
+    '@babel/plugin-transform-react-jsx',
+  ],
+};
