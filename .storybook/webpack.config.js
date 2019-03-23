a9f5d3dd0700adf8d7ffdfc7094f79bd32be85ab
Andrea Carraro <me@andreacarraro.it>
2019-03-23 18:39:47 +0100

    Get rid of babel-core

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -5,11 +5,10 @@ module.exports = ({ config, mode }) => {
   // Manually add "svg-partial-circle" to the paths to be transpiled
   config.module.rules.map(rule => {
     if (rule.test.test('.jsx')) {
-      if (Array.isArray(rule.include) === false) {
+      if (!Array.isArray(rule.include)) {
         rule.include = [];
       }
       rule.include.push(path.join(ROOT, 'node_modules', 'svg-partial-circle'));
-      delete rule.exclude;
     }
     return rule;
   });
