193f4df2d2806a0b391dedc0bb9348eed6ee738c
Andrea Carraro <me@andreacarraro.it>
2019-06-02 00:57:24 +0200

    Add tests and examples for props.data[i].style

--- src/propTypes.js
+++ src/propTypes.js
@@ -1,14 +1,15 @@
 import PropTypes from 'prop-types';
 
+export const stylePropType = PropTypes.objectOf(
+  PropTypes.oneOfType([PropTypes.number, PropTypes.string])
+);
+
 export const dataPropType = PropTypes.arrayOf(
   PropTypes.shape({
     title: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
     value: PropTypes.number.isRequired,
-    key: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
     color: PropTypes.string,
+    key: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    style: stylePropType,
   })
-);
-
-export const stylePropType = PropTypes.objectOf(
-  PropTypes.oneOfType([PropTypes.number, PropTypes.string])
 );
