78eedef823c55d5075c4e7bbf9ccaf651c9e91a7
Andrea Carraro <me@andreacarraro.it>
2019-06-07 18:16:31 +0200

    Switch from .babelrc to babel.config.js

---
+++ babel.config.js
@@ -0,0 +1,17 @@
+/* global process, module */
+const test = process.env.NODE_ENV === 'test';
+module.exports = api => {
+  api.cache(false);
+  return {
+    presets: [
+      [
+        '@babel/env',
+        {
+          modules: test ? 'cjs' : false,
+          loose: true,
+        },
+      ],
+    ],
+    plugins: ['@babel/plugin-transform-react-jsx'],
+  };
+};
