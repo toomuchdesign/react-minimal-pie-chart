3184f322d740c2bcfd05921e3ad3c54c57272857
Andrea Carraro <me@andreacarraro.it>
2020-03-26 11:19:20 +0100

    Update Storybook config

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,45 +1,38 @@
 import React from 'react';
-import { addDecorator, addParameters, configure } from '@storybook/react';
-import {
-  setDefaults as setInfoDefaults,
-  withInfo,
-} from '@storybook/addon-info';
-
-function ContainDecorator(story) {
-  return (
-    <div
-      style={{
-        maxWidth: '400px',
-        margin: '0 auto',
-        padding: '50px',
-      }}
-    >
-      {story()}
-    </div>
-  );
-}
-
-setInfoDefaults({
-  header: false,
-  inline: false,
-  propTables: false,
-  maxPropsIntoLine: 1,
-  inline: true,
-});
+import { addDecorator, addParameters } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
 
 addParameters({
   options: {
-    name: 'React minimal pie chart',
-    url: 'https://github.com/toomuchdesign/react-minimal-pie-chart',
+    theme: {
+      brandTitle: 'React minimal pie chart',
+      brandUrl: 'https://github.com/toomuchdesign/react-minimal-pie-chart',
+    },
     showPanel: false,
     panelPosition: 'right',
   },
 });
-addDecorator((story, context) => withInfo()(story)(context));
+
+addDecorator(
+  withInfo({
+    header: false,
+    inline: false,
+    propTables: false,
+    maxPropsIntoLine: 1,
+    inline: true,
+  })
+);
 addDecorator(story => (
   <div>
     {story()}
-    <p>
+    <p
+      style={{
+        color: '#333',
+        fontSize: '13px',
+        textAlign: 'center',
+        fontFamily: 'Helvetica Neue,Helvetica,Arial,sans-serif',
+      }}
+    >
       Find the original source of the examples{' '}
       <a href="https://github.com/toomuchdesign/react-minimal-pie-chart/blob/master/stories/index.js">
         here.
@@ -47,6 +40,14 @@ addDecorator(story => (
     </p>
   </div>
 ));
-addDecorator(ContainDecorator);
-
-configure(() => require('../stories/index.js'), module);
+addDecorator(story => (
+  <div
+    style={{
+      maxWidth: '400px',
+      margin: '0 auto',
+      padding: '50px',
+    }}
+  >
+    {story()}
+  </div>
+));
