2e18bcbe182667c34ce58974170774d6abe0af04
Andrea Carraro <me@andreacarraro.it>
2020-04-15 14:36:46 +0200

    Update demos

--- stories/PartialLoadingIndicatorStory.js
+++ stories/PartialLoadingIndicatorStory.js
@@ -1,47 +1,33 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PieChart from '../src';
 
-class PartialLoadingIndicatorStory extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      percentage: 20,
-    };
-
-    this.handleRangeChange = this.handleRangeChange.bind(this);
-  }
-
-  handleRangeChange(e) {
-    const newValue = e.target.value;
-    this.setState(() => ({ percentage: Number(newValue) }));
-  }
-
-  render() {
-    return (
-      <div>
-        <PieChart
-          data={[{ value: 1, key: 1, color: '#E38627' }]}
-          reveal={this.state.percentage}
-          lineWidth={20}
-          background="#bfbfbf"
-          lengthAngle={270}
-          rounded
-          animate
-        />
-        Reveal: {this.state.percentage}%
-        <input
-          type="range"
-          min="0"
-          max="100"
-          step="1"
-          value={this.state.percentage}
-          style={{ width: '100%' }}
-          onChange={this.handleRangeChange}
-        />
-      </div>
-    );
-  }
+function PartialLoadingIndicatorStory() {
+  const [percentage, setPercentage] = useState(20);
+  return (
+    <div>
+      <PieChart
+        data={[{ value: 1, key: 1, color: '#E38627' }]}
+        reveal={percentage}
+        lineWidth={20}
+        background="#bfbfbf"
+        lengthAngle={270}
+        rounded
+        animate
+      />
+      Reveal: {percentage}%
+      <input
+        type="range"
+        min="0"
+        max="100"
+        step="1"
+        value={percentage}
+        style={{ width: '100%' }}
+        onChange={(e) => {
+          setPercentage(Number(e.target.value));
+        }}
+      />
+    </div>
+  );
 }
 
 export default PartialLoadingIndicatorStory;
