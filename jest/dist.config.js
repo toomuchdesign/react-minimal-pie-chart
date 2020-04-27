ea85d7ff61a6b761d075b5f527c91a317b67ec1e
Andrea Carraro <me@andreacarraro.it>
2020-04-27 11:06:03 +0200

    Fix test setup for bundled artefacts

--- jest/dist.config.js
+++ jest/dist.config.js
@@ -3,6 +3,6 @@ var pkg = require('../package.json');
 module.exports = Object.assign({}, pkg.jest, {
   rootDir: '../',
   moduleNameMapper: {
-    '/src/index$': '<rootDir>/dist/index',
+    '/src$': '<rootDir>/dist/index',
   },
 });
--- jest/lib.config.js
+++ jest/lib.config.js
@@ -3,6 +3,6 @@ var pkg = require('../package.json');
 module.exports = Object.assign({}, pkg.jest, {
   rootDir: '../',
   moduleNameMapper: {
-    '/src/index$': '<rootDir>/lib/index',
+    '/src$': '<rootDir>/lib/index',
   },
 });
