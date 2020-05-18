f1c76b6d311fcf554809aac27c0e36799e0f141d
Andrea Carraro <me@andreacarraro.it>
2020-05-18 18:37:21 +0200

    Remove @storybook/addon-info

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,45 +1,16 @@
 import React from 'react';
-import { addDecorator, addParameters } from '@storybook/react';
-import { withInfo } from '@storybook/addon-info';
+import { addDecorator } from '@storybook/react';
+import { addons } from '@storybook/addons';
 
-addParameters({
-  options: {
-    theme: {
-      brandTitle: 'React minimal pie chart',
-      brandUrl: 'https://github.com/toomuchdesign/react-minimal-pie-chart',
-    },
-    showPanel: false,
-    panelPosition: 'right',
+addons.setConfig({
+  theme: {
+    brandTitle: 'React minimal pie chart',
+    brandUrl: 'https://github.com/toomuchdesign/react-minimal-pie-chart',
   },
+  showPanel: false,
+  panelPosition: 'right',
 });
 
-addDecorator(
-  withInfo({
-    header: false,
-    inline: false,
-    propTables: false,
-    maxPropsIntoLine: 1,
-    inline: true,
-  })
-);
-addDecorator((story) => (
-  <div>
-    {story()}
-    <p
-      style={{
-        color: '#333',
-        fontSize: '13px',
-        textAlign: 'center',
-        fontFamily: 'Helvetica Neue,Helvetica,Arial,sans-serif',
-      }}
-    >
-      Find the original source of the examples{' '}
-      <a href="https://github.com/toomuchdesign/react-minimal-pie-chart/blob/master/stories/index.js">
-        here.
-      </a>
-    </p>
-  </div>
-));
 addDecorator((story) => (
   <div
     style={{
@@ -49,5 +20,19 @@ addDecorator((story) => (
     }}
   >
     {story()}
+    <p
+      style={{
+        color: '#333',
+        fontSize: '13px',
+        textAlign: 'center',
+        fontFamily: 'Helvetica Neue,Helvetica,Arial,sans-serif',
+      }}
+    >
+      Read the source of this demo{' '}
+      <a href="https://github.com/toomuchdesign/react-minimal-pie-chart/blob/master/stories/index.tsx">
+        here
+      </a>
+      .
+    </p>
   </div>
 ));
