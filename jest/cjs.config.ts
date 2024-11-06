2e7196de475a3368b726b644d6f80bb7e3a4e8a7
Andrea Carraro <me@andreacarraro.it>
2024-11-06 22:17:08 +0100

    test: use jest.config.ts

--- jest/cjs.config.ts
+++ jest/cjs.config.ts
@@ -1,6 +1,6 @@
-import pkg from '../package.json';
+import config from '../jest.config';
 
-module.exports = Object.assign({}, pkg.jest, {
+module.exports = Object.assign({}, config, {
   rootDir: '../',
   moduleNameMapper: {
     '/src$': '<rootDir>/dist/index.cjs.js',
